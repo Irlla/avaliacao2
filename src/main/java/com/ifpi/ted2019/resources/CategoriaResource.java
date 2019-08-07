f97f01a7c32b1ffc142975d49a3f1489592a168d
Irlla <irllalopes07@gmail.com>
2019-08-07 13:25:21 -0300

    Primeiro Commit

---
+++ src/main/java/com/ifpi/ted2019/resources/CategoriaResource.java
@@ -0,0 +1,10 @@
+package com.ifpi.ted2019.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (value= "/funcionario")
+public class CategoriaResource {
+
+}
